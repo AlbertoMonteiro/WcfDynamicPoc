5cd85dead44c0d8ca81acfcb0c2c111cbc5b6a20
Alberto Monteiro <alberto.monteiro@live.com>
2018-04-28 01:39:18 -0300

    Add project files.

---
+++ WcfDynamicPoc/ICorreios.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace WcfDynamicPoc
+{
+    [ServiceContract(Namespace = "http://cliente.bean.master.sigep.bsb.correios.com.br/")]
+    [XmlSerializerFormat(Style = OperationFormatStyle.Rpc, Use = OperationFormatUse.Literal)]
+    public interface ICorreios
+    {
+        [OperationContract(Action = "", ReplyAction = "*")]
+        [return: MessageParameter(Name = "return")]
+        string consultaCEP(string cep);
+        //void consultaCEP([XmlElement( Namespace = "http://cliente.bean.master.sigep.bsb.correios.com.br/")] XmlNode cep);
+
+        //[OperationContract(Action = "", ReplyAction = "*")]
+        //[XmlSerializerFormat(SupportFaults = true)]
+        //ConsultaCepResponse PegaDados(ConsultaCep cep);
+    }
+
+    [MessageContract(WrapperName = "consultaCEP", WrapperNamespace = "http://cliente.bean.master.sigep.bsb.correios.com.br/", IsWrapped = true)]
+    public class ConsultaCep
+    {
+        [MessageBodyMember(Namespace = "http://cliente.bean.master.sigep.bsb.correios.com.br/", Order = 0)]
+        [XmlElement(Form = XmlSchemaForm.Unqualified)]
+        public string cep;
+
+        public ConsultaCep(string cep)
+        {
+            this.cep = cep;
+        }
+    }
+
+    [MessageContract(WrapperName = "consultaCEPResponse", WrapperNamespace = "http://cliente.bean.master.sigep.bsb.correios.com.br/", IsWrapped = true)]
+    public class ConsultaCepResponse
+    {
+        [MessageBodyMember(Namespace = "http://cliente.bean.master.sigep.bsb.correios.com.br/", Order = 0)]
+        [XmlElement("return", Form = XmlSchemaForm.Unqualified)]
+        public Endereco Endereco;
+    }
+
+    [Serializable]
+    [XmlType(Namespace = "http://cliente.bean.master.sigep.bsb.correios.com.br/")]
+    public class Endereco
+    {
+        public override string ToString()
+        {
+            return $"{nameof(bairro)}: {bairro}, {nameof(cep)}: {cep}, {nameof(cidade)}: {cidade}, {nameof(complemento)}: {complemento}, {nameof(complemento2)}: {complemento2}, {nameof(end)}: {end}, {nameof(id)}: {id}, {nameof(uf)}: {uf}";
+        }
+
+        [XmlElement(Form = XmlSchemaForm.Unqualified, Order = 0)]
+        public string bairro { get; set; }
+
+        [XmlElement(Form = XmlSchemaForm.Unqualified, Order = 1)]
+        public uint cep { get; set; }
+
+        [XmlElement(Form = XmlSchemaForm.Unqualified, Order = 2)]
+        public string cidade { get; set; }
+
+        [XmlElement(Form = XmlSchemaForm.Unqualified, Order = 3)]
+        public string complemento { get; set; }
+
+        [XmlElement(Form = XmlSchemaForm.Unqualified, Order = 4)]
+        public string complemento2 { get; set; }
+
+        [XmlElement(Form = XmlSchemaForm.Unqualified, Order = 5)]
+        public string end { get; set; }
+
+        [XmlElement(Form = XmlSchemaForm.Unqualified, Order = 6)]
+        public byte id { get; set; }
+
+        [XmlElement(Form = XmlSchemaForm.Unqualified, Order = 7)]
+        public string uf { get; set; }
+    }
+
+}
---
+++ WcfDynamicPoc/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Web.Services.Description;
+using System.Web.Services.Protocols;
+
+namespace WcfDynamicPoc
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var assembly = GenerateAssemblyFromWsdl("https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl");
+
+            var foundType = assembly.GetTypes().FirstOrDefault(type1 => type1.BaseType == typeof(SoapHttpClientProtocol));
+
+            dynamic atendeCliente = Activator.CreateInstance(foundType);
+
+            var consultaCep = atendeCliente.consultaCEP("60020270");
+            Console.WriteLine(consultaCep);
+        }
+
+        private static Assembly GenerateAssemblyFromWsdl(string wsdlUrl)
+        {
+            var importer = new ServiceDescriptionImporter();
+
+            using (var webClient = new WebClient())
+            {
+                var wsdl = webClient.DownloadString(wsdlUrl);
+                var serviceDescription = ServiceDescription.Read(new StringReader(wsdl));
+                importer.AddServiceDescription(serviceDescription, null, null);
+            }
+            importer.Style = ServiceDescriptionImportStyle.Client;
+
+            importer.CodeGenerationOptions = System.Xml.Serialization.CodeGenerationOptions.GenerateProperties;
+
+            var @namespace = new CodeNamespace();
+            var compilationUnit = new CodeCompileUnit();
+            compilationUnit.Namespaces.Add(@namespace);
+            importer.Import(@namespace, compilationUnit);
+            var cSharpProvider = CodeDomProvider.CreateProvider("C#");
+
+            var parms = new CompilerParameters(new[]
+                {"System.dll", "System.Web.Services.dll", "System.Web.dll", "System.Xml.dll", "System.Data.dll"})
+            {
+                GenerateInMemory = true
+            };
+            var results = cSharpProvider.CompileAssemblyFromDom(parms, compilationUnit);
+            var assembly = results.CompiledAssembly;
+            if (results.Errors.Count > 0)
+            {
+            }
+            return assembly;
+        }
+    }
+}
